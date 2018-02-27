31fc0065929086c6d8676789f8fa66b609d74c14
timothy-spencer <timothy.spencer@gsa.gov>
2018-02-27 14:27:22 -0800

    added a stab at pagerduty support, staging support so I can test this

--- ci/prometheus-down.sh
+++ ci/prometheus-down.sh
@@ -15,7 +15,7 @@
 # this will result in false positives.  Maybe a deploy might do this?
 #
 
-set -ux
+set -u
 
 : ${PROMETHEUSHOST:="0.prometheus.production-monitoring.prometheus-production.toolingbosh"}
 : ${QUERIES:="
@@ -30,23 +30,37 @@ APIOK=yes
 UPDATEOK=no
 
 for QUERY in ${QUERIES} ; do
+  echo "querying prometheus ${PROMETHEUSHOST} for ${QUERY}"
+
   QTIME=$(curl --max-time 5 -s "${PROMETHEUSHOST}":9090/api/v1/query?query="${QUERY}" \
    | jq -r '.data.result[0].value[1]' | sed 's/\..*//')
 
   # make sure that the curl worked (indicates that prometheus is down entirely)
   if [ -z "${QTIME}" ] ; then
+    echo "prometheus API gateway is down!"
     APIOK=no
   else
     # make sure that the data is not too old (indicates that prometheus is not accepting data)
-    TIMEDIFF=$(( ${TIME} - ${QTIME}))
+    TIMEDIFF=$((${TIME} - ${QTIME}))
 
     if [ "${TIMEDIFF}" -lt 600 ] ; then
-     UPDATEOK=yes
+      echo "data for ${QUERY} is less than 600s old"
+      UPDATEOK=yes
+    else
+      echo "data for ${QUERY} is greater than 600s old!"
     fi
   fi
 done
 
-if [ "${APIOK}" != yes -o "${UPDATEOK}" != yes ] ; then
-  # email everybody
-  echo "Terrible news everybody!  Prometheus is down!"
+# exit uncleanly so that the on_failure stuff will trigger a pagerduty alert
+if [ "${APIOK}" != yes ] ; then
+  echo "Terrible news everybody!  Prometheus seems to be down, since I cannot query it's API!"
+  exit 1
 fi
+
+if [ "${UPDATEOK}" != yes ] ; then
+  echo "Terrible news everybody!  Prometheus seems not to be getting new data, and thus is functionally down!"
+  exit 2
+fi
+
+echo "Good news everybody!  Prometheus seems to be UP!"
