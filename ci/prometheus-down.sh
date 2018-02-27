7a04b4861ba38d4cf3d3cafa4a133cc2f7986b19
timothy-spencer <timothy.spencer@gsa.gov>
2018-02-27 09:45:21 -0800

    tried to make shellcheck happy, added job to list, cleaned up syntax that is not needed, moved hostname to variables and made them overridable

--- ci/prometheus-down.sh
+++ ci/prometheus-down.sh
@@ -2,6 +2,8 @@
 # 
 # This script checks a few known queries against the internal prometheus query
 # API.  These queries are chosen because they have timestamps as their values.
+# The queries and the prometheus host can be overridden with environment 
+# variables.
 # 
 # If the query fails, then something is wrong with Prometheus's query API.
 # 
@@ -15,34 +17,36 @@
 
 set -ux
 
-QUERIES="
+: ${PROMETHEUSHOST:="0.prometheus.production-monitoring.prometheus-production.toolingbosh"}
+: ${QUERIES:="
   concourse_has_opslogin_lastcheck
   bosh_unknown_iaas_instance_lastcheck
   bosh_last_scrape_timestamp
   aws_iam_user_lastcheck
-"
-TIME=`date +%s`
+"}
+
+TIME=$(date +%s)
 APIOK=yes
 UPDATEOK=no
 
 for QUERY in ${QUERIES} ; do
-  QTIME=`curl --max-time 5 -s 0.prometheus.production-monitoring.prometheus-production.toolingbosh:9090/api/v1/query?query=${QUERY} \
-   | jq -r '.data.result[0].value[1]' | sed 's/\..*//'`
+  QTIME=$(curl --max-time 5 -s "${PROMETHEUSHOST}":9090/api/v1/query?query="${QUERY}" \
+   | jq -r '.data.result[0].value[1]' | sed 's/\..*//')
 
   # make sure that the curl worked (indicates that prometheus is down entirely)
-  if [ -z ${QTIME} ] ; then
+  if [ -z "${QTIME}" ] ; then
     APIOK=no
   else
     # make sure that the data is not too old (indicates that prometheus is not accepting data)
-    TIMEDIFF=`expr ${TIME} - ${QTIME}`
+    TIMEDIFF=$(( "${TIME}" - "${QTIME}"))
 
-    if [ ${TIMEDIFF} -lt 600 ] ; then
+    if [ "${TIMEDIFF}" -lt 600 ] ; then
      UPDATEOK=yes
     fi
   fi
 done
 
-if [ ${APIOK} != yes -o ${UPDATEOK} != yes ] ; then
+if [ "${APIOK}" != yes -o "${UPDATEOK}" != yes ] ; then
   # email everybody
   echo "Terrible news everybody!  Prometheus is down!"
 fi
