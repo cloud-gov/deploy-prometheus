b06d1fb423793ef918574daaccf6a05447e74dc0
Tim Spencer <timothy.spencer@gsa.gov>
2018-02-28 14:15:32 -0800

    added prometheus down alert (#70)
    * added prometheus down metric
    
    * tried to make shellcheck happy, added job to list, cleaned up syntax that is not needed, moved hostname to variables and made them overridable
    
    * fix quoting so that the expr actually works
    
    * added a stab at pagerduty support, staging support so I can test this
    
    * made the output a bit more readable, cleaned up the shell math
    
    * apparently, I don't need the resource line
    
    * cleaned up some stuff that fly didn't like
    
    * handle case when data is missing entirely
    
    * removed previous pagerduty notification and wrote my own
    
    * as per feedback in the PR
    
    * needed to change API message too
    
    * argh.  forgot to change more logic.  This time, it's tested.
    
    * fixed up un-needed resource stuff in staging too
    
    * fixed alert source for staging

---
+++ ci/prometheus-down.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# 
+# This script checks a few known queries against the internal prometheus query
+# API.  These queries are chosen because they have timestamps as their values.
+# The queries and the prometheus host can be overridden with environment 
+# variables.
+# 
+# If the query fails, then something is wrong with Prometheus's query API.
+# 
+# If at least one timestamp is up to date, then we know it's probably working,
+# otherwise, something is jammed up inside probably.
+# 
+# 
+# XXX If something stops the processes that generate these timestamp metrics,
+# this will result in false positives.  Maybe a deploy might do this?
+#
+
+set -u
+
+: ${PROMETHEUSHOST:="0.prometheus.production-monitoring.prometheus-production.toolingbosh"}
+: ${QUERIES:="
+  concourse_has_opslogin_lastcheck
+  bosh_unknown_iaas_instance_lastcheck
+  bosh_last_scrape_timestamp
+  aws_iam_user_lastcheck
+"}
+
+TIME=$(date +%s)
+APIOK=no
+UPDATEOK=no
+
+for QUERY in ${QUERIES} ; do
+  echo "querying prometheus ${PROMETHEUSHOST} for ${QUERY}"
+
+  QTIME=$(curl --max-time 5 -s "${PROMETHEUSHOST}":9090/api/v1/query?query="${QUERY}" \
+   | jq -r '.data.result[0].value[1]' | sed 's/\..*//')
+
+
+  # make sure that the curl worked (indicates that prometheus is down entirely)
+  if [ ! -z "${QTIME}" ] ; then
+    echo "  prometheus API gateway is UP!"
+    APIOK=yes
+  
+    if [ "${QTIME}" = "null" ] ; then
+      echo "  API is OK, but no data for ${QUERY}"
+    else
+      # make sure that the data is not too old (indicates that prometheus is not accepting data)
+      TIMEDIFF=$((TIME - QTIME))
+
+      if [ "${TIMEDIFF}" -lt 600 ] ; then
+        echo "  data for ${QUERY} is less than 600s old"
+        UPDATEOK=yes
+      else
+        echo "  data for ${QUERY} is greater than 600s old!"
+      fi
+    fi
+  fi
+done
+
+# exit uncleanly so that the on_failure stuff will trigger a pagerduty alert
+if [ "${APIOK}" != yes ] ; then
+  echo "Terrible news everybody!  Prometheus seems to be down, since I cannot query it's API!"
+  exit 1
+fi
+
+if [ "${UPDATEOK}" != yes ] ; then
+  echo "Terrible news everybody!  Prometheus seems not to be getting new data, and thus is functionally down!"
+  exit 2
+fi
+
+echo "Good news everybody!  Prometheus seems to be UP!"
