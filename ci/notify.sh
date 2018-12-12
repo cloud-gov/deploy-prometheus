2bfefdca499427cd042cd75a5b7cfce094ec920f
Roger Steve Ruiz <hi@rog.gr>
2018-12-12 15:49:30 -0500

    Have prometheus also send alerts to cg-notifications Google Group (#104)
    * Getting started by bringing in the properties;
    
    I'm reading through the docs here and here:
    - https://github.com/bosh-prometheus/prometheus-boshrelease/blob/master/jobs/alertmanager/spec#L71-L84
    - https://prometheus.io/docs/alerting/configuration/#<email_config>
    
    * Change around unnecessary properties
    
    * Remove the hello property
    
    * Add resource and resource_type
    
    * Update certificates for postfix server
    
    * Clarify templates
    
    * Fix interpolation
    
    * Update to gsa domain
    
    * Migrate old pagerduty-notify to notify;
    
    The idea here is that notify would both create output for the
    email-resource and also run the pagerduty curl commands.
    
    * Add ca_cert property
    
    * Delete files we no longer need
    
    * Add notify to prometheus checks for email delivery
    
    * Add params to tasks; (#105)
    
    Concourse complains about this as warnings and eventually the tasks will
    start to fail. So let's get ahead of it.

--- ci/notify.sh
+++ ci/notify.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# 
+#
+
 # This script sends an alert to pagerduty using the api key in PAGERDUTY_KEY and
 # the message in ALERT_MESSAGE.
-# 
+#
 set -eu
 
 URL="https://events.pagerduty.com/v2/enqueue"
@@ -25,3 +26,16 @@ curl -H "Content-Type: application/json" \
 	-X POST \
     -d "${DATA}" \
     "${URL}"
+
+# This script creates a subject and body output for
+# Concourse to send out emails using the `email-resource`.
+cat <<EOF >> email/subject.txt
+[${ALERT_SEVERITY}] ${ALERT_MESSAGE}
+EOF
+
+cat <<EOF >> email/body.txt
+summary: ${ALERT_MESSAGE}
+source: ${ALERT_SOURCE}
+severity: ${ALERT_SEVERITY}
+component: ${ALERT_COMPONENT}
+EOF
