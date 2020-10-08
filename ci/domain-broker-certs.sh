555b463cccb3713120e91a4126150ab9abd4fe7c
Ben Berry <ben.berry@gsa.gov>
2020-10-08 14:08:37 -0700

    add test for ELB certificate expiration

--- ci/domain-broker-certs.sh
+++ ci/domain-broker-certs.sh
@@ -19,18 +19,29 @@ done
 
 nlbs=0
 ncerts=0
+cert_names=""
 for lb_arn in "${lb_arns[@]}"; do
   lb_listener_arns=$(aws elbv2 describe-listeners --load-balancer-arn "${lb_arn}" \
       | jq -r ".Listeners[] | select(.Port == 443) | .ListenerArn")
-    for lb_listener_arn in ${lb_listener_arns}; do
-      nlbs=$((nlbs + 1))
-      ncerts_listener=$(aws elbv2 describe-listener-certificates --listener-arn "${lb_listener_arn}" \
-          | jq -r ".Certificates | length")
-      ncerts=$((ncerts + ncerts_listener))
+  for lb_listener_arn in ${lb_listener_arns}; do
+    nlbs=$((nlbs + 1))
+    certs_listener=$(aws elbv2 describe-listener-certificates --listener-arn "${lb_listener_arn}")
+    cert_names="${cert_names}"$'\n'"$(echo ${certs_listener} | jq -r '.Certificates[] | .CertificateArn'  | awk -F/ '{ print $NF }')"
+    ncerts_listener=$(echo "${certs_listener}" | jq -r ".Certificates | length")
+    ncerts=$((ncerts + ncerts_listener))
   done
 done
+cert_expirations=""
+for cert_name in ${cert_names}; do
+  cert_metadata=$(aws iam get-server-certificate --server-certificate-name ${cert_name})
+  cert_id=$(echo "${cert_metadata}" | jq -r '.ServerCertificate | .ServerCertificateMetadata | .ServerCertificateId')
+  cert_date=$(echo "${cert_metadata}" | jq -r '.ServerCertificate | .ServerCertificateMetadata | .Expiration')
+  cert_expiration=$(date --date "${cert_date}" +%s)
+  cert_expirations="${cert_expirations}"$'\n'"domain_broker_certificate_expiration{certificate_name=\"${cert_name}\"} ${cert_expiration}"
+done
 
-cat <<EOF | curl --data-binary @- "${GATEWAY_HOST}:${GATEWAY_PORT:-9091}/metrics/job/domain_broker/instance/${ENVIRONMENT}"
+cat <<EOF  | curl --data-binary @- "${GATEWAY_HOST}:${GATEWAY_PORT:-9091}/metrics/job/domain_broker/instance/${ENVIRONMENT}"
 domain_broker_listener_count ${nlbs}
 domain_broker_certificate_count ${ncerts}
+${cert_expirations}
 EOF
