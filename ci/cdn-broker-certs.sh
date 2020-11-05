efc2f2c550ce3c8ece249935b26d02acdf547a7e
Andrew Burnes <andrew.burnes@gsa.gov>
2020-11-05 13:49:04 -0700

    Add CDN domain name to openssl client check

--- ci/cdn-broker-certs.sh
+++ ci/cdn-broker-certs.sh
@@ -4,11 +4,12 @@ set -euxo pipefail
 
 ## Calculate the days to expiration from now
 function days_to_cert_expiration {
-  local target=$1;
-  local date_today=$(date +%s)
+  local client=$1;
+  local server=$2;
+  local date_today=$(date +%s);
 
   ## Get expirationdate and convert to epoch
-  expirationdate=$(date -d "$(: | openssl s_client -connect $target:443 -servername $target 2>/dev/null \
+  expirationdate=$(date -d "$(: | openssl s_client -connect $client:443 -servername $server 2>/dev/null \
     | openssl x509 -text \
     | grep 'Not After' \
     | awk '{print $4,$5,$7}')" '+%s');
@@ -29,22 +30,24 @@ function days_to_cert_expiration {
 
 ## Select and get Cloudfront instances returning ARN, & Aliases
 alias_count=0
-cdns_selector='.DistributionList.Items[] | select(.Aliases.Items != null) | {id:.Id,arn:.ARN,aliases:.Aliases.Items}'
+cdn_count=0
+cdns_selector='.DistributionList.Items[] | select(.Aliases.Items != null) | {id:.Id,arn:.ARN,aliases:.Aliases.Items,domain:.DomainName}'
 cdns=$(aws cloudfront list-distributions | jq -c "${cdns_selector}")
-cdn_count=$(echo $cdn | jq -r "length")
 cdn_list=($cdns)
 
 ## Loop through list of CDN instances
 cdn_certificate_expirations=""
 for cdn in "${cdn_list[@]}"; do
+  cdn_count=$((cdn_count + 1))
   aliases=($(echo $cdn | jq -r ".aliases[]"))
   arn=($(echo $cdn | jq -r ".arn"))
+  domain=($(echo $cdn | jq -r ".domain"))
   id=($(echo $cdn | jq -r ".id"))
 
   for alias in "${aliases[@]}"; do
     alias_count=$((alias_count + 1))
-    days_to_expire=$(days_to_cert_expiration $alias)
-    cdn_certificate_expirations="${cdn_certificate_expirations}"$'\n'"cdn_certificate_expiration{id=\"${id}\",arn=\"${arn}\",alias=\"${alias}\"} ${days_to_expire}"
+    days_to_expire=$(days_to_cert_expiration $domain $alias)
+    cdn_certificate_expirations="${cdn_certificate_expirations}"$'\n'"cdn_certificate_expiration{id=\"${id}\",arn=\"${arn}\",alias=\"${alias}\",domain=\"${domain}\"} ${days_to_expire}"
   done
 done
 
