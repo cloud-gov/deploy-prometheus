9e56d0c1ef46c10348596a8ead802d19e11fe45c
Robert Gottlieb <robert.gottlieb@gsa.gov>
2023-12-06 16:00:39 -0500

    more debug

--- ci/aws-iam-check-keys/find_stale_keys.py
+++ ci/aws-iam-check-keys/find_stale_keys.py
@@ -312,7 +312,7 @@ def format_user_dicts(users_list, thresholds):
     new_dict = {}
     user_list = []
     for key in users_list:
-        print("thresholds: {thresholds}")
+        print(f'thresholds: {thresholds}')
         found_thresholds = [dict for dict in thresholds if dict['account_type'] == "Operators"]
         if len(found_thresholds) > 0:
             found_threshold = found_thresholds[0]
@@ -349,7 +349,7 @@ def load_tf_users(tf_filename, thresholds):
         if "username" in key:
             #if key not in tf_users:
             # , "is_wildcard": False, "alert": True, "warn": 75, "violation": 90 }
-            print("thresholds: {thresholds}")
+            print(f"thresholds: {thresholds}")
             found_thresholds = [dict for dict in thresholds if dict['account_type'] == "Platform"] 
             if len(found_thresholds) > 0:
                 found_threshold = found_thresholds[0]
