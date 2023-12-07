bb027d0e36f88ef14e0feef443569f8af3814e7b
Robert Gottlieb <robert.gottlieb@gsa.gov>
2023-12-07 10:20:42 -0500

    possible fix for wildcard matches, and debug print

--- ci/aws-iam-check-keys/find_stale_keys.py
+++ ci/aws-iam-check-keys/find_stale_keys.py
@@ -76,7 +76,7 @@ def find_known_user(report_user, all_users_dict):
 
     user_dict = {}
     for an_user_dict in all_users_dict:
-        if an_user_dict["is_wildcard"] and report_user in an_user_dict['user'] or an_user_dict['user'] == report_user:
+        if (an_user_dict["is_wildcard"] == True and report_user in an_user_dict['user']) or an_user_dict['user'] == report_user:
             user_dict = an_user_dict
             break
     if user_dict == {}:
@@ -387,10 +387,13 @@ def main():
         print(f'searching profile {com_key}')
         all_com_users = com_users_list + tf_users + other_users
         search_for_keys(com_region, com_state_dict[com_key], all_com_users)
-
+    i = 1
     for gov_key in gov_state_dict:
         print(f'searching profile {gov_key}')
         all_gov_users = gov_users_list + tf_users + other_users
+        if i == 1:
+            print(all_gov_users)
+            i += 1
         search_for_keys(gov_region, gov_state_dict[gov_key], all_gov_users)
     #else:
     #    print("thresholds didn't load, please fix this and try again")
