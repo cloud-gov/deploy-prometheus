8db82050ba1ac2ce667ecb68526001d0f0e77820
Robert Gottlieb <robert.gottlieb@gsa.gov>
2023-12-11 17:38:08 -0500

    testing removing an alert

--- ci/aws-iam-check-keys/find_stale_keys.py
+++ ci/aws-iam-check-keys/find_stale_keys.py
@@ -131,8 +131,12 @@ def check_retention_for_key(access_key_last_rotated, access_key_num, user_row, w
                 # since this is new the alert_sent is set to false. Once an alert is cleared it will be set to true
                 time_stamp = time.time()
                 # stale_key_num {user="cg-broker-***", alert_type="violation", key="1", last_rotated="2022-12-21T163027+0000"} 1
-                print(f'stale_key_num 1 User: {user_row["user"]}-{scrubbed_arn} has an alert of type {alert_type} as the key number {access_key_num} was last rotated: {access_key_last_rotated}\n')
-                prometheus_alerts += f'stale_key_num {{user=\"{user_row["user"]}-{scrubbed_arn}\", alert_type=\"{alert_type}\", key=\"{access_key_num}\", last_rotated=\"{access_key_last_rotated}\"}} 1\n'
+                if user_row['user'] == "cg-s3-7d693173-0e30-4768-86df-d376a50b41d7":
+                    print(f'MARKED_HERE ==== stale_key_num User: {user_row["user"]}-{scrubbed_arn} has an alert of type {alert_type} as the key number {access_key_num} was last rotated: {access_key_last_rotated} 0\n')
+                    prometheus_alerts += f'stale_key_num {{user=\"{user_row["user"]}-{scrubbed_arn}\", alert_type=\"{alert_type}\", key=\"{access_key_num}\", last_rotated=\"{access_key_last_rotated}\"}} 0\n'
+                else:
+                    print(f'stale_key_num 1 User: {user_row["user"]}-{scrubbed_arn} has an alert of type {alert_type} as the key number {access_key_num} was last rotated: {access_key_last_rotated}\n')
+                    prometheus_alerts += f'stale_key_num {{user=\"{user_row["user"]}-{scrubbed_arn}\", alert_type=\"{alert_type}\", key=\"{access_key_num}\", last_rotated=\"{access_key_last_rotated}\"}} 1\n'
                 event.alert_sent = False
                 event.save()
             else:
