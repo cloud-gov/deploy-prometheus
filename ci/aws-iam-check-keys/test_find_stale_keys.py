a5348d0515b9b88fabf229412267b49bbfa26ed5
Robert Gottlieb <robert.gottlieb@gsa.gov>
2024-04-29 09:54:16 -0400

    updated migration, added tests, code cleanup

---
+++ ci/aws-iam-check-keys/test_find_stale_keys.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+import
+
+class Test(TestCase):
+    def test_check_retention(self):
+        self.assertEqual()
+
+    def test_find_known_user(self):
+        self.fail()
+
+    def test_check_retention_for_key(self):
+        self.fail()
+
+    def test_send_alerts(self):
+        self.fail()
+
+    def test_send_all_alerts(self):
+        self.fail()
+
+    def test_check_access_keys(self):
+        self.fail()
+
+    def test_check_user_thresholds(self):
+        self.fail()
+
+    def test_search_for_keys(self):
+        self.fail()
+
+    def test_state_file_to_dict(self):
+        self.fail()
+
+    def test_get_platform_thresholds(self):
+        self.fail()
+
+    def test_format_user_dicts(self):
+        self.fail()
